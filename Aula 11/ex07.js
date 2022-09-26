a8cc5a3fc9537329a3595cecfebbcd85d7f14b96
leobzr <leo72bezerra@gmail.com>
2022-09-26 16:31:00 -0300

    Fazendo um update do que eu tenho feito por aqui
    Aula 11

---
+++ Aula 11/ex07.js
@@ -0,0 +1,6 @@
+var vel = 80
+console.log(`A vloecidade do seu carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida, logo será multado.`)
+}
+console.log(`Vai se foder mlkkkkkkkkk`)
